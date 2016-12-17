1107979b196a7058eb32b66d283cf9e728bcaa67
Cadel Watson <cadel@cadelwatson.com>
2016-12-17 12:54:50 +1100

    Add file selection dialogs for input and output.

--- main.py
+++ main.py
@@ -24,6 +24,18 @@ class GrammarGenApp(QtWidgets.QMainWindow, main_window):
         main_window.__init__(self)
         self.setupUi(self)
 
+        # Connect inputs to handlers
+        self.markdown_path_select.clicked.connect(self.select_markdown_file)
+        self.output_path_select.clicked.connect(self.select_output_file)
+
+    def select_markdown_file(self):
+        filename = QtWidgets.QFileDialog.getOpenFileName(self, 'Select file')
+        self.markdown_path_input.setText(filename[0])
+
+    def select_output_file(self):
+        filename = QtWidgets.QFileDialog.getSaveFileName(self, 'Create file')
+        self.output_path_input.setText(filename[0])
+
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
