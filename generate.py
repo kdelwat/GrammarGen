08ad3a47692868503ac39ed11c86eed5a2e7fadd
Cadel Watson <cadel@cadelwatson.com>
2016-12-17 14:15:45 +1100

    Add theme support

--- generate.py
+++ generate.py
@@ -1,8 +1,14 @@
+import os
 import pypandoc
 
 filters = ['filter.py']
+pandoc_arguments = ['--standalone', '--toc']
 
 
 def generate(markdown, theme='Default'):
     '''Takes a markdown string and returns a full HTML document, ready to save.'''
-    return pypandoc.convert_text(markdown, 'html', format='md', filters=filters)
+    css_location = os.path.join('themes', '{0}.css'.format(theme))
+    pandoc_arguments.append('--css={0}'.format(css_location))
+
+    return pypandoc.convert_text(markdown, 'html', format='md',
+                                 extra_args=pandoc_arguments, filters=filters)
