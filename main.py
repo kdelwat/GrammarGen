872669ffaaba1b6203c6b3e2e6a3c152855f60ed
Cadel Watson <cadel@cadelwatson.com>
2016-12-17 13:30:12 +1100

    Add progress bar for generation

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import sys
+import math
+import time
 
 from PyQt5 import QtCore, QtGui, QtWidgets, uic
 import pypandoc
@@ -26,10 +28,30 @@ class GrammarGenApp(QtWidgets.QMainWindow, main_window):
         main_window.__init__(self)
         self.setupUi(self)
 
+        # Clear progress bar
+        self.clear_progress(stages=4)
+
         # Connect inputs to handlers
         self.markdown_path_select.clicked.connect(self.select_markdown_file)
         self.output_path_select.clicked.connect(self.select_output_file)
         self.generate_button.clicked.connect(self.generate)
+
+    def clear_progress(self, stages):
+        '''Set the progress bar to 0. Calculate the number of intervals needed
+        based on the number of generation stages and add the correct increment to
+        a variable.
+        '''
+        self.progress = 0
+        self.generate_progress.setValue(self.progress)
+
+        self.increment = math.ceil(100 / stages)
+
+    def update_progress(self, message):
+        '''Increment the progress bar one step, displaying the given status
+        message.'''
+        self.progress += self.increment
+        self.generate_progress.setValue(self.progress)
+        self.status_bar.showMessage(message)
 
     def select_markdown_file(self):
         filename = QtWidgets.QFileDialog.getOpenFileName(self, 'Select file')
@@ -41,6 +63,16 @@ class GrammarGenApp(QtWidgets.QMainWindow, main_window):
 
     def generate(self):
         '''Generate the HTML file.'''
+        self.update_progress('Loading Markdown...')
+        time.sleep(1)
+        self.update_progress('Generating HTML...')
+        time.sleep(1)
+        self.update_progress('Styling output...')
+        time.sleep(1)
+        self.update_progress('Saving HTML...')
+        time.sleep(1)
+        self.clear_progress(4)
+
         markdown_filename = self.markdown_path_input.text()
         output_filename = self.output_path_input.text()
 
